47d5fe0c703066531627723165364f6636ba9bd2
Doori Kim <doori.alice.kim@gmail.com>
2020-06-18 22:28:25 +0900

    kfc wip

---
+++ src/Pages/Landing.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div className="landing-container">
+      <img
+        src="https://cdn.pixabay.com/photo/2016/09/16/19/15/gear-1674891_960_720.png"
+        alt="factory"
+      ></img>
+    </div>
+  );
+};
+
+export default Landing;
---
+++ src/Routes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Landing from "./Pages/Landing";
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default Routes;

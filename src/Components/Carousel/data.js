cc1341bd1d76ab06832608494cf5e711b499a4c4
Doori Kim <doori.alice.kim@gmail.com>
2020-06-28 23:42:14 +0900

    color slider styling completed

--- src/Components/Carousel/data.js
+++ src/Components/Carousel/data.js
@@ -35,6 +35,102 @@ const data = [
     name: 'Serenity',
     year: 2016,
   },
+  {
+    color: '#945153',
+    number: '18-1438',
+    name: 'Marsala',
+    year: 2015,
+  },
+  {
+    color: '#AD5E98',
+    number: '18-3224',
+    name: 'Radiant Orchild',
+    year: 2014,
+  },
+  {
+    color: '#029B77',
+    number: '17-5641',
+    name: 'Emerald',
+    year: 2013,
+  },
+  {
+    color: '#EF5441',
+    number: '17-1463',
+    name: 'Tangerine Tango',
+    year: 2012,
+  },
+  {
+    color: '#DA4F70',
+    number: '18-2120',
+    name: 'Honeysuckle',
+    year: 2011,
+  },
+  {
+    color: '#46B4A9',
+    number: '15-5519',
+    name: 'Turquoise',
+    year: 2010,
+  },
+  {
+    color: '#EEBD57',
+    number: '14-0848',
+    name: 'Mimosa',
+    year: 2009,
+  },
+  {
+    color: '#585E9E',
+    number: '18-3943',
+    name: 'Blue Iris',
+    year: 2008,
+  },
+  {
+    color: '#991E35',
+    number: '19-1557',
+    name: 'Chill Pepper',
+    year: 2007,
+  },
+  {
+    color: '#DFCDC1',
+    number: '13-1106',
+    name: 'Sand Dollar',
+    year: 2006,
+  },
+  {
+    color: '#54AFAC',
+    number: '15-5217',
+    name: 'Blue Turquoise',
+    year: 2005,
+  },
+  {
+    color: '#E25944',
+    number: '17-1456',
+    name: 'Tigerlily',
+    year: 2004,
+  },
+  {
+    color: '#7EC6C5',
+    number: '14-4811',
+    name: 'Aqua Sky',
+    year: 2003,
+  },
+  {
+    color: '#BF2439',
+    number: '19-1664',
+    name: 'True Red',
+    year: 2002,
+  },
+  {
+    color: '#C64676',
+    number: '17-2031',
+    name: 'Fuchsia Rose',
+    year: 2001,
+  },
+  {
+    color: '#9EB7D4',
+    number: '15-4020',
+    name: 'Cerulean',
+    year: 2000,
+  },
 
 ];
 
--- src/Components/Carousel/index.js
+++ src/Components/Carousel/index.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import ImgCard from './ImgCard';
 import data from './data';
+import './index.scss';
 
 const Carousel = props => {
   const {
     children,
   } = props;
 
+  const onClickMove = e => {
+
+  };
+
   return (
     <>
-      <div className="carousel-container" style={{ display: 'flex' }}>
-        {data.map(el => (
-          <ImgCard color={el.color} number={el.number} name={el.name} year={el.year} key={el.name} />
+      <div className="carousel-container">
+        <span className="carousel-title">Past Colors of the Year</span>
+        <div className="divider" />
+        <div className="carousel-contents">
+          <div className="carousel-card-container">
+            {data.map(el => (
+              <ImgCard color={el.color} number={el.number} name={el.name} year={el.year} key={el.name} />
 
-        ))}
+            ))}
+          </div>
+          <div className="swiper-container">
+            <span>{'<'}</span>
+            <span>{'>'}</span>
+          </div>
+        </div>
       </div>
     </>
   );

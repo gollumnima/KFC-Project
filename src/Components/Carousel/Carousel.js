b6cb6212c3bf8c0ef9528eef35fd870e656dbad4
Doori Kim <doori.alice.kim@gmail.com>
2021-02-02 23:45:29 +0900

    WIP : 약간의 에러 수정했지만 에러가 더 남아있음

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -12,40 +12,35 @@ const Carousel = props => {
   const [current, setCurrent] = useState(0);
   const [currentSlide, setCurrentSlide] = useState([]);
 
-  console.log(data.length);
-
   useEffect(() => {
-    setCurrentSlide(data.slice(0, slidesToShow));
-  }, []);
+    setCurrentSlide(data.slice(current, current + slidesToShow));
+  }, [data, current, slidesToShow]);
 
   const handleClick = e => {
     const { className } = e.target;
-    console.log(current, 'cu');
     if (current < 0) {
       setCurrent(0);
     }
-    if (current > data.length) {
+    if (current >= data.length) {
       setCurrent(data.length - slidesToShow);
     }
     return className.includes('right') ? setCurrent(current + slidesToScroll) : setCurrent(current - slidesToScroll);
   };
-  console.log(current, 'cu');
 
-  useEffect(() => {
-    const start = current;
-    const end = current + slidesToShow;
-    console.log(current, 'startpoint');
-    console.log(end % slidesToShow, 'end 나머지');
-
-    if (end % slidesToScroll !== 0) {
-      setCurrentSlide(data.slice(data.length - slidesToScroll, data.length));
-    } else {
-      setCurrentSlide(data.slice(start, end));
-    }
-  }, [current]);
+  // !autoPlay && (useEffect(() => {
+  //   const start = current;
+  //   const end = current + slidesToShow;
+  //   console.log(current, 'startpoint');
+  //   console.log(end % slidesToShow, 'end 나머지');
+  //   if (end % slidesToScroll !== 0) {
+  //     setCurrentSlide(data.slice(data.length - slidesToScroll, data.length));
+  //   } else {
+  //     setCurrentSlide(data.slice(start, end));
+  //   }
+  // }, [current]))();
 
   const handleAutoPlay = () => {
-    const nextIdx = (current + 1) % data.length;
+    const nextIdx = (current + slidesToShow) % data.length;
 
     setTimeout(() => {
       setCurrent(nextIdx);
@@ -53,25 +48,28 @@ const Carousel = props => {
     }, speed);
   };
 
+  console.log(current, 'current');
   useEffect(() => {
-    setInterval(() => {
-      if (autoPlaySpeed) handleAutoPlay();
-    //   setCurrent(end);
-    //   if (end >= data.length) {
-    //     setCurrentSlide(data.length - slidesToScroll, data.length);
-    //   } else {
-    //     setCurrentSlide(data.slice(start, end));
-    //   }
-    }, 4000);
-  }, [current]);
+    autoPlaySpeed && setInterval(
 
-  console.log({ current });
+      () => handleAutoPlay(),
+
+      //   setCurrent(end);
+      //   if (end >= data.length) {
+      //     setCurrentSlide(data.length - slidesToScroll, data.length);
+      //   } else {
+      //     setCurrentSlide(data.slice(start, end));
+      //   }
+      300000000,
+    );
+  }, [current]);
 
   return (
     <div className="carousel">
       <button
         className="carousel__left"
-        onClick={e => !autoPlay && handleClick(e)}
+        type="button"
+        onClick={e => !!autoPlay || handleClick(e)}
       />
       <div
         className="carousel__wrapper"
--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from 'src/Pages/Landing';
-import CarouselOriginal from 'src/Components/Carousel/CarouselOriginal';
 import InputsTest from 'src/Components/Inputs/InputsTest';
 import CarouselTest from 'src/Components/Carousel/CarouselTest';
 
@@ -9,9 +8,9 @@ const Routes = () => (
   <Router>
     <Switch>
       <Route exact path="/" component={Landing} />
-      <Route path="/carousel" component={CarouselOriginal} />
-      <Route path="/carousel2" component={CarouselTest} />
+      <Route path="/carousel" component={CarouselTest} />
       <Route path="/inputs" component={InputsTest} />
+
     </Switch>
   </Router>
 );
